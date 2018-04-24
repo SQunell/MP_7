c7a298aedaac12d6558ebbcd9628b14c731d90e6
jcpopp <jcpopp2@illinois.edu>
2018-04-24 00:07:22 -0500

    Added basic functionality to the app.

--- app/src/main/java/edu/illinois/cs/cs125/SQunell_MP_7/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/SQunell_MP_7/MainActivity.java
@@ -1,4 +1,7 @@
 package edu.illinois.cs.cs125.SQunell_MP_7;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,10 +21,47 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+
 /**
  * Main class for MP.
  */
 public final class MainActivity extends AppCompatActivity {
+
+    /**
+     * Opens up the camera to take a picture
+     */
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+   //Sets the image view to the photo that was just taken
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            pic.setImageBitmap(imageBitmap);
+        }
+    }
+
+
+
+
+
+    ImageView pic;
+    Button submit;
+    Button photo;
+    TextView recs;
+
+    //Temporary variable- Just used to switch between two preset recommended food options
+    int counter;
+
+
     /** Default logging tag for messages from the main activity. */
     private static final String TAG = "MP_7:";
 
@@ -44,16 +84,8 @@ public final class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-
-        final Button photo = findViewById(R.id.Photo);
-        photo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(final View v) {
-                Log.d(TAG, "take photo button clicked");
-                //TAKE PHOTO METHOD
-            }
-        });
         final Button importz = findViewById(R.id.Import);
+        importz.setVisibility(View.VISIBLE);
         importz.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View v) {
@@ -61,41 +93,102 @@ public final class MainActivity extends AppCompatActivity {
                 //IMPORT PHOTO METHOD
             }
         });
-        final ImageView pic = findViewById(R.id.Image);
-        final Button submit = findViewById(R.id.Submit);
-        submit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(final View v) {
-                Log.d(TAG, "Submit photo button clicked");
-                //ANALYZE PHOTO METHOD
-            }
-        });
 
-        final TextView recs = findViewById(R.id.Recommend);
         final Button newfood = findViewById(R.id.Refood);
+        newfood.setVisibility(View.INVISIBLE);
         newfood.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View v) {
                 Log.d(TAG, "New food button clicked");
                 //NEW FOOD METHOD
+                if (counter % 2 == 0) {
+                    recs.setText("Emotion: Sadness    Recommended Food: Mac and Cheese");
+                } else {
+                    recs.setText("Emotion: Sadness    Recommended Food: Ice Cream");
+                }
+                counter++;
             }
         });
-        final Button restaurant = findViewById(R.id.Yes);
+
+        final Button restaurant = findViewById(R.id.Explore);
+        restaurant.setVisibility(View.INVISIBLE);
         restaurant.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View v) {
-                Log.d(TAG, "Find restaurant button clicked");
-                //FIND RESTAURANT METHOD
+                Log.d(TAG, "Explore restaurant button clicked");
+
+                if (counter % 2 == 0) {
+                    recs.setText("Recommended Restaurant: Oberweis Dairy");
+                } else {
+                    recs.setText("Recommended Restaurant: Noodles & Company");
+                }
             }
         });
+
         final Button redo = findViewById(R.id.Redo);
+        redo.setVisibility(View.INVISIBLE);
         redo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View v) {
                 Log.d(TAG, "redo photo button clicked");
                 //RETURN TO STARTING STATE/VISIBILITY
+
+                newfood.setVisibility(View.INVISIBLE);
+                restaurant.setVisibility(View.INVISIBLE);
+                redo.setVisibility(View.INVISIBLE);
+                recs.setVisibility(View.INVISIBLE);
+                importz.setVisibility(View.VISIBLE);
+                photo.setVisibility(View.VISIBLE);
+                submit.setVisibility(View.INVISIBLE);
+                pic.setVisibility(View.INVISIBLE);
+                recs.setText("Emotion: Sadness    Recommended Food: Ice Cream");
+                counter = 0;
             }
         });
+
+        recs = findViewById(R.id.Recommend);
+        recs.setVisibility(View.INVISIBLE);
+
+        photo = findViewById(R.id.Photo);
+        photo.setVisibility(View.VISIBLE);
+        photo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                Log.d(TAG, "take photo button clicked");
+                //TAKE PHOTO METHOD
+                dispatchTakePictureIntent();
+                pic.setVisibility(View.VISIBLE);
+                submit.setVisibility(View.VISIBLE);
+
+            }
+        });
+
+        submit = findViewById(R.id.Submit);
+        submit.setVisibility(View.INVISIBLE);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                Log.d(TAG, "Submit photo button clicked");
+                //ANALYZE PHOTO METHOD
+                newfood.setVisibility(View.VISIBLE);
+                restaurant.setVisibility(View.VISIBLE);
+                redo.setVisibility(View.VISIBLE);
+                recs.setVisibility(View.VISIBLE);
+                importz.setVisibility(View.INVISIBLE);
+                photo.setVisibility(View.INVISIBLE);
+                submit.setVisibility(View.INVISIBLE);
+
+            }
+        });
+
+
+
+        pic = findViewById(R.id.Image);
+        pic.setVisibility(View.INVISIBLE);
+
+
+
+
 
 
     }
